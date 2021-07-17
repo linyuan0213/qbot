198ab0f70f7ceef0644c47f12c278b1d1e41e811
linyuan0213 <linyuan213@gmail.com>
2021-07-17 23:05:51 +0800

    fix: bugfix

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -3,17 +3,17 @@ from utils import bytes_utils
 
 def test_bytes():
     speed_b = 1023
-    speed_b_ch = bytes.bytes_to_human(speed_b)
+    speed_b_ch = bytes_utils.bytes_to_human(speed_b)
     assert speed_b_ch == '1023B'
 
     speed_kb = 1024 * 1
-    speed_kb_ch = bytes.bytes_to_human(speed_kb)
+    speed_kb_ch = bytes_utils.bytes_to_human(speed_kb)
     assert speed_kb_ch == '1.0KB'
 
     speed_mb = 1024 * 1024 * 1
-    speed_mb_ch = bytes.bytes_to_human(speed_mb)
+    speed_mb_ch = bytes_utils.bytes_to_human(speed_mb)
     assert speed_mb_ch == '1.0MB'
 
     speed_gb = 1024 * 1024 * 1024 * 1
-    speed_gb_ch = bytes.bytes_to_human(speed_gb)
+    speed_gb_ch = bytes_utils.bytes_to_human(speed_gb)
     assert speed_gb_ch == '1.0GB'
